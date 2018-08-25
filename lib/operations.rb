1a405a90b2319dabada9e7bd0a649f8efd92a00f
Michael Pieschel <sixtyhectares@yahoo.com>
2018-08-25 18:50:14 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,15 +1,15 @@
 def unsafe?(speed)
   if speed <= 60 && speed >= 40
-    True
-  else
     FALSE
+  else
+    TRUE
   end
 end
 
 
 
 def not_safe?(speed)
-  speed <= 60 && speed >= 40 ? TRUE : FALSE
+  speed <= 60 && speed >= 40 ? FALSE : TRUE
 end
 	
 
